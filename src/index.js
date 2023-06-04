d4235252a65e9e642c4da2c55e5dea9e05df93d5
Varun Barad <varun@varunbarad.com>
2023-06-04 19:21:53 +0530

    Pull data from database

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
 require('dotenv').config();
 
 const express = require('express');
+const database = require('pg-promise')()(process.env.DATABASE_URL);
+
 const app = express();
 const port = 3000;
 
-app.get('/v1/events', (request, response) => {
+const pg = require('pg');
+pg.types.setTypeParser(pg.types.builtins.DATE, (stringValue) => {
+	return stringValue;
+});
+
+app.get('/v1/events', async (request, response) => {
 	const queryOffset = request.query.offset || null;
 
+	const events = await database.any('select * from events');
+
 	response.json({
-		events: [],
+		events: events.map((event) => ({
+			id: event.id,
+			source: event.source,
+			start: {
+				date: event.start_date,
+				time: event.start_time || null,
+			},
+			end: {
+				date: event.end_date || null,
+				time: event.end_time || null,
+			},
+			name: event.name,
+			backlink: event.backlink,
+			location: event.location,
+			price: event.price,
+			genre: event.genre.map((genre) => genre.toLowerCase()),
+		})),
 		nextOffset: null,
 		previousOffset: queryOffset,
 	});
