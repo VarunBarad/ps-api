085e1c3748d435185f04fd5398d48877e7407613
Varun Barad <varun@varunbarad.com>
2023-06-04 11:59:39 +0530

    Add a simple API on port 3000

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/v1/events', (request, response) => {
+	const queryOffset = request.query.offset || null;
+
+	response.json({
+		events: [],
+		nextOffset: null,
+		previousOffset: queryOffset,
+	});
+});
+
+// Handle 404
+app.use((req, res, next) => {
+	res.sendStatus(404);
+});
+
+app.listen(port, () => {
+	console.log(`App running on port ${port}.`);
+});
